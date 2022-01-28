ee4bc68767b3a3b114c5d7753f702d7c78b3fe33
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-01-28 21:41:45 +0900

    refactor(preset_tw): use internal color by util

--- preset_tw/rules/typography.ts
+++ preset_tw/rules/typography.ts
@@ -1,13 +1,8 @@
 import { isNumber, isString } from "../../deps.ts";
 import { resolveTheme } from "../../core/utils/resolver.ts";
-import { SEPARATOR } from "../_utils.ts";
-import { hex2RGBA, parseNumeric } from "../../core/utils/parse.ts";
-import { stringifyRGBA, stringifyVar } from "../../core/utils/color.ts";
-import { constructVar } from "../../core/_utils.ts";
+import { colorByProp, colorOpacityByProp } from "./_utils.ts";
 import type { PresetTwTheme } from "../theme/types.ts";
 import type { Rule } from "../../core/types.ts";
-
-const variablePrefix = "map-";
 
 export const lineHeights: Rule[] = [
   [/^leading-(.+)$/, ([, path], { theme }) => {
@@ -24,53 +19,10 @@ export const lineHeights: Rule[] = [
 ];
 
 export const colors: Rule[] = [
-  [/^text-(.+)\/(\d+)$/, ([, body, op], { theme }) => {
-    const colors = body.split(SEPARATOR);
-
-    const color = resolveTheme(theme as PresetTwTheme, {
-      scope: "color",
-      path: colors,
-    }) as unknown;
-    if (!isString(color)) return;
-
-    const maybeRGBA = hex2RGBA(color);
-    const opacity = parseNumeric(op);
-
-    if (!maybeRGBA || !isNumber(opacity)) return;
-
-    const { r, g, b } = maybeRGBA;
-
-    return {
-      color: stringifyRGBA({ r, g, b, a: opacity / 100 }),
-    };
+  [/^text-(.+)\/(\d+)$/, ([, prop, opacity], { theme }) => {
+    return colorOpacityByProp("color", { theme, prop, opacity });
   }],
-  [/^text-(.+)$/, ([, body], { theme }) => {
-    const colors = body.split(SEPARATOR);
-
-    const color = resolveTheme(theme as PresetTwTheme, {
-      scope: "color",
-      path: colors,
-    }) as unknown;
-    if (!isString(color)) return;
-
-    const maybeRGBA = hex2RGBA(color);
-    if (!maybeRGBA) {
-      return {
-        color,
-      };
-    }
-    const { r, g, b, a } = maybeRGBA;
-    const opacity = constructVar("text-opacity", variablePrefix);
-
-    if (isNumber(a)) {
-      return {
-        color: stringifyRGBA({ r, g, b, a: a / 100 }),
-      };
-    }
-
-    return {
-      [opacity]: 1,
-      color: stringifyRGBA({ r, g, b, a: stringifyVar(opacity) }),
-    };
+  [/^text-(.+)$/, ([, prop], { theme }) => {
+    return colorByProp("color", { theme, prop });
   }],
 ];
