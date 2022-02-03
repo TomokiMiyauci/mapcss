64fe1057adc57cafb0df2859f63096912e7a90b4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-04 01:30:53 +0900

    feat(preset_tw): add modifier for breakpoints

--- preset_tw/modifiers/breakpoint.ts
+++ preset_tw/modifiers/breakpoint.ts
@@ -1,21 +1,36 @@
-import type { ModifierHandler } from "./../../core/types.ts";
+import type { GlobalModifier } from "./../../core/types.ts";
 
-export const sm: ModifierHandler = () => ({
-  ruleSet: (ruleset) => `@media (min-width: 640px) {\n${ruleset}\n}`,
-});
+export const sm: GlobalModifier = {
+  type: "global",
+  fn: () => ({
+    ruleSet: (ruleSet) => `@media (min-width: 640px) {\n${ruleSet}\n}`,
+  }),
+};
 
-export const md: ModifierHandler = () => ({
-  ruleSet: (ruleset) => `@media (min-width: 768px) {\n${ruleset}\n}`,
-});
+export const md: GlobalModifier = {
+  type: "global",
+  fn: () => ({
+    ruleSet: (ruleSet) => `@media (min-width: 768px) {\n${ruleSet}\n}`,
+  }),
+};
 
-export const lg: ModifierHandler = () => ({
-  ruleSet: (ruleset) => `@media (min-width: 1024px) {\n${ruleset}\n}`,
-});
+export const lg: GlobalModifier = {
+  type: "global",
+  fn: () => ({
+    ruleSet: (ruleSet) => `@media (min-width: 1024px) {\n${ruleSet}\n}`,
+  }),
+};
 
-export const xl: ModifierHandler = () => ({
-  ruleSet: (ruleset) => `@media (min-width: 1280px) {\n${ruleset}\n}`,
-});
+export const xl: GlobalModifier = {
+  type: "global",
+  fn: () => ({
+    ruleSet: (ruleSet) => `@media (min-width: 1280px) {\n${ruleSet}\n}`,
+  }),
+};
 
-export const $2xl: ModifierHandler = () => ({
-  ruleSet: (ruleset) => `@media (min-width: 1536px) {\n${ruleset}\n}`,
-});
+export const $2xl: GlobalModifier = {
+  type: "global",
+  fn: () => ({
+    ruleSet: (ruleSet) => `@media (min-width: 1536px) {\n${ruleSet}\n}`,
+  }),
+};
