be0911f418c52a10be6d7475e6ab2894ae357e8c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-06 00:32:07 +0900

    chore(preset_tw): add specifier of grid-cols-*

--- preset_tw/specifier/grid.ts
+++ preset_tw/specifier/grid.ts
@@ -1,3 +1,14 @@
+import { rePositiveNumber } from "../../core/utils/regexp.ts";
+import { numericBy } from "./_utils.ts";
 import type { Specifier } from "../../core/types.ts";
 
-export const grid: Specifier = [["DEFAULT", { display: "grid" }]];
+export const grid: Specifier = [["DEFAULT", { display: "grid" }], ["cols", [[
+  "none",
+  { "grid-template-columns": "none" },
+], [rePositiveNumber, ([, pNumber]) =>
+  numericBy(
+    pNumber,
+    (number) => ({
+      "grid-template-columns": `repeat(${number}, minmax(0, 1fr))`,
+    }),
+  )]]]];
