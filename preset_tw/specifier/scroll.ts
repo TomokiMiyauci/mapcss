617aac6d0f3b0e6c59e1ff26746b6efba1a6047a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-09 22:46:15 +0900

    feat(preset_tw): add specifier for `scroll-margin`

--- preset_tw/specifier/scroll.ts
+++ preset_tw/specifier/scroll.ts
@@ -1,21 +1,108 @@
-import { rePositiveNumber } from "../../core/utils/regexp.ts";
-import { filterValue, handleFilter } from "./_filter_utils.ts";
+import { reNumeric } from "../../core/utils/regexp.ts";
 import { parseNumeric } from "../../core/utils/monad.ts";
-import {
-  customProperty,
-  ratio,
-  shortDecimal,
-  unit,
-} from "../../core/utils/format.ts";
-import type {
-  CSSObject,
-  EntriesSpecifier,
-  RecordSpecifier,
-  Specifier,
-} from "../../core/types.ts";
-import { handleTransform, transformValue } from "./_utils.ts";
+import { quoter, unit } from "../../core/utils/format.ts";
+import type { EntriesSpecifier } from "../../core/types.ts";
+import { associateWith } from "../../deps.ts";
+
+const SCROLL_MARGIN = "scroll-margin";
+const SCROLL_MARGIN_TOP = "scroll-margin-top";
+const SCROLL_MARGIN_BOTTOM = "scroll-margin-bottom";
+const SCROLL_MARGIN_LEFT = "scroll-margin-left";
+const SCROLL_MARGIN_RIGHT = "scroll-margin-right";
 
 export const scroll: EntriesSpecifier = [
   ["auto", { "scroll-behavior": "auto" }],
   ["smooth", { "scroll-behavior": "smooth" }],
+  ["m", [
+    [0, { [SCROLL_MARGIN]: "0px" }],
+    ["px", { [SCROLL_MARGIN]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) => ({ [SCROLL_MARGIN]: rem }),
+          none: undefined,
+        }),
+    ],
+  ]],
+  ["mx", [
+    [0, { [SCROLL_MARGIN_LEFT]: "0px", [SCROLL_MARGIN_RIGHT]: "0px" }],
+    ["px", { [SCROLL_MARGIN_LEFT]: "1px", [SCROLL_MARGIN_RIGHT]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) =>
+            associateWith(
+              [SCROLL_MARGIN_LEFT, SCROLL_MARGIN_RIGHT],
+              () => rem,
+            ),
+          none: undefined,
+        }),
+    ],
+  ]],
+  ["my", [
+    [0, { [SCROLL_MARGIN_TOP]: "0px", [SCROLL_MARGIN_BOTTOM]: "0px" }],
+    ["px", { [SCROLL_MARGIN_TOP]: "1px", [SCROLL_MARGIN_BOTTOM]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) =>
+            associateWith(
+              [SCROLL_MARGIN_TOP, SCROLL_MARGIN_BOTTOM],
+              () => rem,
+            ),
+          none: undefined,
+        }),
+    ],
+  ]],
+  ["mt", [
+    [0, { [SCROLL_MARGIN_TOP]: "0px" }],
+    ["px", { [SCROLL_MARGIN_TOP]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) => ({ [SCROLL_MARGIN_TOP]: rem }),
+          none: undefined,
+        }),
+    ],
+  ]],
+  ["mr", [
+    [0, { [SCROLL_MARGIN_RIGHT]: "0px" }],
+    ["px", { [SCROLL_MARGIN_RIGHT]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) => ({ [SCROLL_MARGIN_RIGHT]: rem }),
+          none: undefined,
+        }),
+    ],
+  ]],
+  ["mb", [
+    [0, { [SCROLL_MARGIN_BOTTOM]: "0px" }],
+    ["px", { [SCROLL_MARGIN_BOTTOM]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) => ({ [SCROLL_MARGIN_BOTTOM]: rem }),
+          none: undefined,
+        }),
+    ],
+  ]],
+  ["ml", [
+    [0, { [SCROLL_MARGIN_LEFT]: "0px" }],
+    ["px", { [SCROLL_MARGIN_LEFT]: "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        parseNumeric(numeric).map(quoter).map(unit("rem")).match({
+          some: (rem) => ({ [SCROLL_MARGIN_LEFT]: rem }),
+          none: undefined,
+        }),
+    ],
+  ]],
 ];
