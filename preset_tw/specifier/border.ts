426c6a0e014f2136e4115daf51b3d9378ab9d250
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-10 17:36:53 +0900

    feat(preset_tw): add arbitary rule to `border-color`

--- preset_tw/specifier/border.ts
+++ preset_tw/specifier/border.ts
@@ -1,7 +1,14 @@
-import { isUndefined } from "../../deps.ts";
-import { resolveTheme } from "../../core/utils/resolver.ts";
-import { reAll, reNumeric, reSlashNumber } from "../../core/utils/regexp.ts";
 import { associatePx, associateRGBA, pxBy } from "./_utils.ts";
+import { resolveTheme } from "../../core/utils/resolver.ts";
+import { associateWith, isUndefined } from "../../deps.ts";
+import {
+  re$SlashBracket$,
+  reAll,
+  reNumeric,
+  reSlashNumber,
+} from "../../core/utils/regexp.ts";
+import { parseColor, parseNumeric } from "../../core/utils/monad.ts";
+import { completionRGBA, ratio, rgbFn } from "../../core/utils/format.ts";
 import type { Specifier } from "../../core/types.ts";
 
 const BORDER_WIDTH = "border-width";
@@ -16,132 +23,377 @@ export const border: Specifier = [
   ["none", { "border-style": "none" }],
   ["collapse", { "border-collapse": "collapse" }],
   ["separate", { "border-collapse": "separate" }],
-  ["x", [["DEFAULT", {
-    "border-left-width": "1px",
-    "border-right-width": "1px",
-  }], [
-    reNumeric,
-    ([, numeric]) =>
-      associatePx(numeric, ["border-left-width", "border-right-width"]),
-  ], [reSlashNumber, ([, body, alpha], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
+  ["x", [
+    ["DEFAULT", {
+      "border-left-width": "1px",
+      "border-right-width": "1px",
+    }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        associatePx(numeric, ["border-left-width", "border-right-width"]),
+    ],
+    [reSlashNumber, ([, body, numeric], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
 
-    return associateRGBA(
-      color,
-      ["border-left-color", "border-right-color"],
-      alpha,
-    );
-  }], [reAll, ([body], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-left-color", "border-right-color"]);
-  }]]],
-  ["y", [["DEFAULT", {
-    "border-top-width": "1px",
-    "border-bottom-width": "1px",
-  }], [reSlashNumber, ([, body, alpha], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
+      return parseNumeric(numeric).match({
+        some: (number) =>
+          parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match(
+            {
+              some: (color) =>
+                associateWith(
+                  ["border-right-color", "border-left-color"],
+                  () => color,
+                ),
+              none: undefined,
+            },
+          ),
+        none: undefined,
+      });
+    }],
+    [re$SlashBracket$, ([, body, alpha], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+      return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha }))
+        .map(
+          rgbFn,
+        ).match({
+          some: (color) =>
+            associateWith(
+              ["border-right-color", "border-left-color"],
+              () => color,
+            ),
+          none: undefined,
+        });
+    }],
+    [
+      reAll,
+      ([body], context) => {
+        const color = resolveTheme(body, "color", context);
+        if (isUndefined(color)) return;
 
-    return associateRGBA(
-      color,
-      ["border-top-color", "border-bottom-color"],
-      alpha,
-    );
-  }], [
-    reNumeric,
-    ([, numeric]) =>
-      associatePx(numeric, ["border-top-width", "border-bottom-width"]),
-  ], [reAll, ([body], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-top-color", "border-bottom-color"]);
-  }]]],
-  ["t", [["DEFAULT", { "border-top-width": "1px" }], [
-    reSlashNumber,
-    ([, body, alpha], context) => {
+        return parseColor(color).map(completionRGBA(1, true))
+          .map(rgbFn)
+          .match({
+            some: (color) =>
+              associateWith(
+                ["border-right-color", "border-left-color"],
+                () => color,
+              ),
+            none: () =>
+              associateWith(
+                ["border-right-color", "border-left-color"],
+                () => color,
+              ),
+          });
+      },
+    ],
+  ]],
+  ["y", [
+    ["DEFAULT", {
+      "border-top-width": "1px",
+      "border-bottom-width": "1px",
+    }],
+    [reSlashNumber, ([, body, alpha], context) => {
       const color = resolveTheme(body, "color", context);
       if (isUndefined(color)) return;
 
       return associateRGBA(
         color,
-        ["border-top-color", "border-top-color"],
+        ["border-top-color", "border-bottom-color"],
         alpha,
       );
-    },
-  ], [
-    reNumeric,
-    ([, numeric]) => associatePx(numeric, ["border-top-width"]),
-  ], [reAll, ([body], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-top-color"]);
-  }]]],
-  ["r", [["DEFAULT", { "border-right-width": "1px" }], [
-    reNumeric,
-    ([, numeric]) => associatePx(numeric, ["border-right-width"]),
-  ], [reSlashNumber, ([, body, alpha], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
+    }],
+    [
+      reNumeric,
+      ([, numeric]) =>
+        associatePx(numeric, ["border-top-width", "border-bottom-width"]),
+    ],
+    [reSlashNumber, ([, body, numeric], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
 
-    return associateRGBA(
-      color,
-      ["border-right-color"],
-      alpha,
-    );
-  }], [reAll, ([body], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-right-color"]);
-  }]]],
-  ["b", [["DEFAULT", { "border-bottom-width": "1px" }], [
-    reNumeric,
-    ([, numeric]) => associatePx(numeric, ["border-bottom-width"]),
-  ], [reSlashNumber, ([, body, alpha], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
+      return parseNumeric(numeric).match({
+        some: (number) =>
+          parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match(
+            {
+              some: (color) =>
+                associateWith(
+                  ["border-top-color", "border-bottom-color"],
+                  () => color,
+                ),
+              none: undefined,
+            },
+          ),
+        none: undefined,
+      });
+    }],
+    [re$SlashBracket$, ([, body, alpha], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+      return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha }))
+        .map(
+          rgbFn,
+        ).match({
+          some: (color) =>
+            associateWith(
+              ["border-top-color", "border-bottom-color"],
+              () => color,
+            ),
+          none: undefined,
+        });
+    }],
+    [
+      reAll,
+      ([body], context) => {
+        const color = resolveTheme(body, "color", context);
+        if (isUndefined(color)) return;
 
-    return associateRGBA(
-      color,
-      ["border-bottom-color"],
-      alpha,
-    );
-  }], [reAll, ([body], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-bottom-color"]);
-  }]]],
-  ["l", [["DEFAULT", { "border-left-width": "1px" }], [
-    reNumeric,
-    ([, numeric]) => associatePx(numeric, ["border-left-width"]),
-  ], [reSlashNumber, ([, body, alpha], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
+        return parseColor(color).map(completionRGBA(1, true))
+          .map(rgbFn)
+          .match({
+            some: (color) =>
+              associateWith(
+                ["border-top-color", "border-bottom-color"],
+                () => color,
+              ),
+            none: () =>
+              associateWith(
+                ["border-top-color", "border-bottom-color"],
+                () => color,
+              ),
+          });
+      },
+    ],
+  ]],
+  ["t", [
+    ["DEFAULT", { "border-top-width": "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) => associatePx(numeric, ["border-top-width"]),
+    ],
+    [reSlashNumber, ([, body, numeric], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
 
-    return associateRGBA(
-      color,
-      ["border-left-color"],
-      alpha,
-    );
-  }], [reAll, ([body], context) => {
-    const color = resolveTheme(body, "color", context);
-    if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-left-color"]);
-  }]]],
+      return parseNumeric(numeric).match({
+        some: (number) =>
+          parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match(
+            {
+              some: (color) => ({ "border-top-color": color }),
+              none: undefined,
+            },
+          ),
+        none: undefined,
+      });
+    }],
+    [re$SlashBracket$, ([, body, alpha], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+      return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha }))
+        .map(
+          rgbFn,
+        ).match({
+          some: (color) => ({ "border-top-color": color }),
+          none: undefined,
+        });
+    }],
+    [
+      reAll,
+      ([body], context) => {
+        const color = resolveTheme(body, "color", context);
+        if (isUndefined(color)) return;
+
+        return parseColor(color).map(completionRGBA(1, true))
+          .map(rgbFn)
+          .match({
+            some: (color) => ({ "border-top-color": color }),
+            none: ({ "border-top-color": color }),
+          });
+      },
+    ],
+  ]],
+  ["r", [
+    ["DEFAULT", { "border-right-width": "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) => associatePx(numeric, ["border-right-width"]),
+    ],
+    [reSlashNumber, ([, body, numeric], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+
+      return parseNumeric(numeric).match({
+        some: (number) =>
+          parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match(
+            {
+              some: (color) => ({ "border-right-color": color }),
+              none: undefined,
+            },
+          ),
+        none: undefined,
+      });
+    }],
+    [re$SlashBracket$, ([, body, alpha], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+      return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha }))
+        .map(
+          rgbFn,
+        ).match({
+          some: (color) => ({ "border-right-color": color }),
+          none: undefined,
+        });
+    }],
+    [
+      reAll,
+      ([body], context) => {
+        const color = resolveTheme(body, "color", context);
+        if (isUndefined(color)) return;
+
+        return parseColor(color).map(completionRGBA(1, true))
+          .map(rgbFn)
+          .match({
+            some: (color) => ({ "border-right-color": color }),
+            none: ({ "border-right-color": color }),
+          });
+      },
+    ],
+  ]],
+  ["b", [
+    ["DEFAULT", { "border-bottom-width": "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) => associatePx(numeric, ["border-bottom-width"]),
+    ],
+    [re$SlashBracket$, ([, body, alpha], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+      return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha }))
+        .map(
+          rgbFn,
+        ).match({
+          some: (color) => ({ "border-bottom-color": color }),
+          none: undefined,
+        });
+    }],
+    [reSlashNumber, ([, body, numeric], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+
+      return parseNumeric(numeric).match({
+        some: (number) =>
+          parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match(
+            {
+              some: (color) => ({ "border-bottom-color": color }),
+              none: undefined,
+            },
+          ),
+        none: undefined,
+      });
+    }],
+    [
+      reAll,
+      ([body], context) => {
+        const color = resolveTheme(body, "color", context);
+        if (isUndefined(color)) return;
+
+        return parseColor(color).map(completionRGBA(1, true))
+          .map(rgbFn)
+          .match({
+            some: (color) => ({ "border-bottom-color": color }),
+            none: ({ "border-bottom-color": color }),
+          });
+      },
+    ],
+  ]],
+  ["l", [
+    ["DEFAULT", { "border-left-width": "1px" }],
+    [
+      reNumeric,
+      ([, numeric]) => associatePx(numeric, ["border-left-width"]),
+    ],
+    [reSlashNumber, ([, body, numeric], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+
+      return parseNumeric(numeric).match({
+        some: (number) =>
+          parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match(
+            {
+              some: (color) => ({ "border-left-color": color }),
+              none: undefined,
+            },
+          ),
+        none: undefined,
+      });
+    }],
+    [re$SlashBracket$, ([, body, alpha], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+      return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha }))
+        .map(
+          rgbFn,
+        ).match({
+          some: (color) => ({ "border-left-color": color }),
+          none: undefined,
+        });
+    }],
+    [
+      reAll,
+      ([body], context) => {
+        const color = resolveTheme(body, "color", context);
+        if (isUndefined(color)) return;
+
+        return parseColor(color).map(completionRGBA(1, true))
+          .map(rgbFn)
+          .match({
+            some: (color) => ({ "border-left-color": color }),
+            none: ({ "border-left-color": color }),
+          });
+      },
+    ],
+  ]],
   [
     reNumeric,
     ([, numeric]) => pxBy(numeric, (number) => ({ [BORDER_WIDTH]: number })),
   ],
-  [reSlashNumber, ([, body, alpha], context) => {
+  [reSlashNumber, ([, body, numeric], context) => {
     const color = resolveTheme(body, "color", context);
     if (isUndefined(color)) return;
 
-    return associateRGBA(color, ["border-color"], alpha);
+    return parseNumeric(numeric).match({
+      some: (number) =>
+        parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match({
+          some: (color) => ({ "border-color": color }),
+          none: undefined,
+        }),
+      none: undefined,
+    });
   }],
-  [reAll, ([body], context) => {
+  [re$SlashBracket$, ([, body, alpha], context) => {
     const color = resolveTheme(body, "color", context);
     if (isUndefined(color)) return;
-    return associateRGBA(color, ["border-color"]);
+    return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha })).map(
+      rgbFn,
+    ).match({
+      some: (color) => ({ "border-color": color }),
+      none: undefined,
+    });
   }],
+  [
+    reAll,
+    ([body], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+
+      return parseColor(color).map(completionRGBA(1, true))
+        .map(rgbFn)
+        .match({
+          some: (color) => ({ "border-color": color }),
+          none: ({ "border-color": color }),
+        });
+    },
+  ],
 ];
