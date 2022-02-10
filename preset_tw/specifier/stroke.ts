a5ffbb0f9dc3aeb01ebdf9167eb07c851fb2f99c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-10 22:59:19 +0900

    feat(preset_tw): add specifier for `stroke`

--- preset_tw/specifier/stroke.ts
+++ preset_tw/specifier/stroke.ts
@@ -1,6 +1,18 @@
 import { rePositiveNumber } from "../../core/utils/regexp.ts";
-import { parseNumeric } from "../../core/utils/monad.ts";
+import { resolveTheme } from "../../core/utils/resolver.ts";
+import { isUndefined } from "../../deps.ts";
+import {
+  re$SlashBracket$,
+  reAll,
+  reSlashNumber,
+} from "../../core/utils/regexp.ts";
+import { parseColor, parseNumeric } from "../../core/utils/monad.ts";
+import { completionRGBA, ratio, rgbFn } from "../../core/utils/format.ts";
 import type { EntriesSpecifier } from "../../core/types.ts";
+
+function toStroke(color: string) {
+  return { stroke: color };
+}
 
 export const stroke: EntriesSpecifier = [
   [rePositiveNumber, ([, pNumber]) =>
@@ -8,4 +20,42 @@ export const stroke: EntriesSpecifier = [
       some: (number) => ({ "stroke-width": number }),
       none: undefined,
     })],
+
+  [reSlashNumber, ([, body, numeric], context) => {
+    const color = resolveTheme(body, "color", context);
+    if (isUndefined(color)) return;
+
+    return parseNumeric(numeric).match({
+      some: (number) =>
+        parseColor(color).map(completionRGBA(ratio(number))).map(rgbFn).match({
+          some: toStroke,
+          none: undefined,
+        }),
+      none: undefined,
+    });
+  }],
+  [re$SlashBracket$, ([, body, alpha], context) => {
+    const color = resolveTheme(body, "color", context);
+    if (isUndefined(color)) return;
+    return parseColor(color).map(({ r, g, b }) => ({ r, g, b, a: alpha })).map(
+      rgbFn,
+    ).match({
+      some: toStroke,
+      none: undefined,
+    });
+  }],
+  [
+    reAll,
+    ([body], context) => {
+      const color = resolveTheme(body, "color", context);
+      if (isUndefined(color)) return;
+
+      return parseColor(color).map(completionRGBA(1, true))
+        .map(rgbFn)
+        .match({
+          some: toStroke,
+          none: () => toStroke(color),
+        });
+    },
+  ],
 ];
