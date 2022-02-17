962f964761aefb412f1bcf1004f70f7d93996b0b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-17 23:41:55 +0900

    docs(_docs): improve aleph plugin

--- _docs/plugins/mapcss.ts
+++ _docs/plugins/mapcss.ts
@@ -1,5 +1,11 @@
-import { type Config as MapCSSConfig, generate } from "../../mod.ts";
+import {
+  type Config as MapCSSConfig,
+  generateStyleSheet,
+  presetTw,
+} from "../../mod.ts";
+import { extractSplit } from "../../core/extractor.ts";
 import type { Plugin } from "aleph/types";
+import { expandGlob, WalkEntry } from "https://deno.land/std@0.125.0/fs/mod.ts";
 
 export type Config = Partial<MapCSSConfig> & {
   /** watch file type
@@ -9,34 +15,56 @@ export type Config = Partial<MapCSSConfig> & {
 };
 
 export default function mapcssPlugin(
-  { ext = ["tsx", "jsx"], ...rest }: Config,
+  { ext = ["tsx", "jsx"] }: Config,
 ): Plugin {
   return {
     name: "mapcss-loader",
-    setup(aleph) {
+    async setup(aleph) {
       const pattern = RegExp(`\.(${ext.join("|")})$`, "i");
-      aleph.onTransform(
-        pattern,
-        async ({ module: { specifier }, code, bundleMode }) => {
-          const url = specifier.replace(pattern, "") + ".map.css";
-          const { css } = generate(
-            rest,
-            code,
-          );
-          if (!css.length) return;
-          const cssModule = await aleph.addModule(url, css, true);
-          const importPath = aleph.resolveImport(
-            cssModule,
-            specifier,
-            bundleMode,
-          );
+      const config = { presets: [presetTw()] };
+      if (aleph.mode === "development") {
+        const baseModule = await aleph.addModule("/style/$map.css", " ");
+        const tokens = new Set<string>([]);
+
+        aleph.onTransform(/^\/app\.tsx$/, ({ code }) => {
+          const path = aleph.resolveImport(baseModule, "/app.tsx");
           return {
-            code: `import "${importPath}";\n${code}`,
-            // support SSR
-            extraDeps: [{ specifier: url, virtual: true }],
+            code: `import "${path}"\n${code}`,
+            extraDeps: [{ specifier: "/style/$map.css", virtual: true }],
           };
-        },
-      );
+        });
+
+        aleph.onTransform(/^(.+)\.tsx|\.mdx$/, ({ code }) => {
+          const _tokens = extractSplit(code);
+          _tokens.forEach((token) => {
+            tokens.add(token);
+          });
+          const { css } = generateStyleSheet(config, tokens);
+
+          const _css = css ? css : " ";
+          aleph.addModule("/style/$map.css", _css, true);
+        });
+      } else if (aleph.mode === "production") {
+        const walkEntry = expandGlob("**/*.{tsx,mdx}");
+
+        const paths = new Set<WalkEntry>();
+        for await (const entry of walkEntry) {
+          if (entry.isFile) {
+            paths.add(entry);
+          }
+        }
+
+        const texts = Array.from(paths).map(({ path }) =>
+          Deno.readTextFileSync(path)
+        );
+        const allCode = texts.reduce((acc, cur) => `${acc}\n${cur}`, "");
+        const { css } = generateStyleSheet(config, allCode);
+        if (!css) return;
+
+        aleph.onRender(({ html }) => {
+          html.head.push(`<style type="text/css">${css}</style>`);
+        });
+      }
     },
   };
 }
--- _docs/plugins/mdx.ts
+++ _docs/plugins/mdx.ts
@@ -5,7 +5,7 @@ import util from "https://deno.land/x/aleph@v0.3.0-beta.19/shared/util.ts";
 import { safeLoadFront } from "https://esm.sh/yaml-front-matter@4.1.1";
 import { walkSync } from "https://deno.land/std@0.122.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
-import { compile } from "https://esm.sh/xdm@1.12.2?deps=unist-util-stringify-position@3.0.0,unist-util-visit@3.0.0";
+import { compile } from "https://esm.sh/@mdx-js/mdx@2.0.0?pin=v66";
 
 export type CompileOptions = Parameters<typeof compile>[1];
 
@@ -152,7 +152,7 @@ export function mdxPlugin(options: CompileOptions): Plugin {
           source,
         );
 
-        const { contents } = await compile(source, options);
+        const { value } = await compile(source, options);
 
         if (framework !== "react") {
           throw new Error(
@@ -162,7 +162,7 @@ export function mdxPlugin(options: CompileOptions): Plugin {
 
         return {
           code: [
-            contents.toString(),
+            value.toString(),
             `MDXContent.meta = ${JSON.stringify(meta)}`,
             `MDXContent.nav = ${JSON.stringify(navMenu)}`,
           ].join("\n"),
