c66321ee23269dfd073fef7b658ef26c83414ee8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-17 23:35:51 +0900

    feat(core): add declaration order processor as default

--- core/post_process.ts
+++ core/post_process.ts
@@ -4,21 +4,40 @@ export const statementOrderProcessor: PostProcessor = {
   name: "statement-order-processor",
   fn: (cssStatements) => {
     return cssStatements.sort((a, b) => {
-      const aMax = Math.abs(Math.max(...a.orders));
-      const bMax = Math.abs(Math.max(...b.orders));
+      const aOrder = a.order;
+      const bOrder = b.order;
 
-      if (Number.isFinite(aMax) || Number.isFinite(bMax)) {
-        if (!Number.isFinite(aMax)) {
-          return -bMax;
+      if (Number.isFinite(aOrder) || Number.isFinite(bOrder)) {
+        if (!Number.isFinite(aOrder)) {
+          return -bOrder;
         }
-        if (!Number.isFinite(bMax)) {
-          return aMax;
+        if (!Number.isFinite(bOrder)) {
+          return aOrder;
         }
 
-        return aMax - bMax;
+        return aOrder - bOrder;
       } else {
         return 0;
       }
+    });
+  },
+};
+
+export const declarationOrderProcessor: PostProcessor = {
+  name: "declaration-order",
+  fn: (cssStatements) => {
+    return cssStatements.map((statement) => {
+      if (statement.type === "ruleset") {
+        const declaration = Object.entries(statement.declaration).sort(
+          ([keyA], [keyB]) => {
+            if (keyA > keyB) return 1;
+            if (keyA < keyB) return -1;
+            return 0;
+          },
+        );
+        return { ...statement, declaration: Object.fromEntries(declaration) };
+      }
+      return statement;
     });
   },
 };
