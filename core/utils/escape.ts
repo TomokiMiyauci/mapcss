f496c638fb34b16c674deeddeb0f2af2e8b3e59c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-18 15:31:16 +0900

    feat(core): improve selector escape function

--- core/utils/escape.ts
+++ core/utils/escape.ts
@@ -1,4 +1,9 @@
-/** Escape special char */
+/** Escape special char
+ * @see https://www.w3.org/TR/CSS21/syndata.html#:~:text=In%20CSS%2C%20identifiers%20(including%20element,hyphen%20followed%20by%20a%20digit.
+ */
 export function escapeRegExp(value: string): string {
-  return value.replaceAll(/[\/\[\]\.\(\),%:!]/g, "\\$&");
+  return value.replaceAll(
+    /[^a-zA-Z\d-_]/g,
+    "\\$&",
+  );
 }
