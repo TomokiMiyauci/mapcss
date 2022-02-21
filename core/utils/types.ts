de6fca1ebfc9475a14b84b80b92419b3b02fb441
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-21 20:31:16 +0900

    refactor(core): remove unused

--- core/utils/types.ts
+++
@@ -1,4 +0,0 @@
-export type Axis = "x" | "y";
-export type Dir = "t" | "r" | "b" | "l";
-export type Direction = "top" | "right" | "bottom" | "left";
-export type Corner = Dir | "tl" | "tr" | "br" | "bl";
