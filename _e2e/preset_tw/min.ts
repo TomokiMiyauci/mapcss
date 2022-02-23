7a3d8599d516a5051c2194ddc93bc5a9f8690e67
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-23 16:54:15 +0900

    chore(_e2e): change test case output format

--- _e2e/preset_tw/min.ts
+++ _e2e/preset_tw/min.ts
@@ -1,9 +1,9 @@
 export const min: [string, string][] = [
-  ["min-h-0", ".min-h-0{min-height:0px;}"],
-  ["min-h-full", ".min-h-full{min-height:100%;}"],
-  ["min-h-screen", ".min-h-screen{min-height:100vh;}"],
-  ["min-h-min", ".min-h-min{min-height:min-content;}"],
-  ["min-h-max", ".min-h-max{min-height:max-content;}"],
-  ["min-h-fit", ".min-h-fit{min-height:fit-content;}"],
-  ["min-h-[50%]", ".min-h-\\[50\\%\\]{min-height:50%;}"],
+  ["min-h-0", ".min-h-0{min-height:0px}"],
+  ["min-h-full", ".min-h-full{min-height:100%}"],
+  ["min-h-screen", ".min-h-screen{min-height:100vh}"],
+  ["min-h-min", ".min-h-min{min-height:min-content}"],
+  ["min-h-max", ".min-h-max{min-height:max-content}"],
+  ["min-h-fit", ".min-h-fit{min-height:fit-content}"],
+  ["min-h-[50%]", ".min-h-\\[50\\%\\]{min-height:50%}"],
 ];
