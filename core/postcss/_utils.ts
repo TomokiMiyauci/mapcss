4407f3642dae090784a648ce595425ce49d71d41
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-25 13:06:38 +0900

    chore(core): add utility for postcss

---
+++ core/postcss/_utils.ts
@@ -0,0 +1,21 @@
+import { Root } from "../../deps.ts";
+
+/** Removes duplicate `decl` nodes in the same `rule`.
+ * The criterion for duplication is that `prop` is equal.
+ * The node that appears first has priority.
+ */
+export function removeDuplicatedDecl(root: Readonly<Root>): Root {
+  const newRoot = root.clone();
+
+  newRoot.walkRules((rule) => {
+    const set = new Set();
+    rule.walkDecls((decl) => {
+      const prop = decl.prop;
+      if (set.has(prop)) {
+        decl.remove();
+      }
+      set.add(prop);
+    });
+  });
+  return newRoot;
+}
---
+++ core/postcss/_utils_test.ts
@@ -0,0 +1,132 @@
+import { removeDuplicatedDecl } from "./_utils.ts";
+
+import { expect, ParamReturn, test } from "../../dev_deps.ts";
+import { Declaration, Root, Rule } from "../../deps.ts";
+
+test("removeDuplicatedDecl", () => {
+  const table: ParamReturn<typeof removeDuplicatedDecl>[] = [
+    [new Root(), new Root()],
+    [
+      new Root({
+        nodes: [
+          new Rule({
+            selector: ".block",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "block",
+              }),
+              new Declaration({
+                prop: "display",
+                value: "inline",
+              }),
+            ],
+          }),
+        ],
+      }),
+      new Root({
+        nodes: [
+          new Rule({
+            selector: ".block",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "block",
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+    [
+      new Root({
+        nodes: [
+          new Rule({
+            selector: ".block",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "block",
+              }),
+              new Declaration({
+                prop: "display",
+                value: "inline",
+              }),
+              new Declaration({
+                prop: "color",
+                value: "red",
+              }),
+            ],
+          }),
+        ],
+      }),
+      new Root({
+        nodes: [
+          new Rule({
+            selector: ".block",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "block",
+              }),
+              new Declaration({
+                prop: "color",
+                value: "red",
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+    [
+      new Root({
+        nodes: [
+          new Rule({
+            selector: ".block",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "block",
+              }),
+            ],
+          }),
+          new Rule({
+            selector: ".inline",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "inline",
+              }),
+            ],
+          }),
+        ],
+      }),
+      new Root({
+        nodes: [
+          new Rule({
+            selector: ".block",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "block",
+              }),
+            ],
+          }),
+          new Rule({
+            selector: ".inline",
+            nodes: [
+              new Declaration({
+                prop: "display",
+                value: "inline",
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+  ];
+
+  table.forEach(([root, result]) =>
+    expect(removeDuplicatedDecl(root).toString()).toEqual(result.toString())
+  );
+});
