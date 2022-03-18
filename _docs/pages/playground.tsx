6c89992cce19757a5bf4279af3da58b1aec920ae
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-18 15:13:08 +0900

    refactor(_docs): rename variable

--- _docs/pages/playground.tsx
+++ _docs/pages/playground.tsx
@@ -89,7 +89,7 @@ export default function Playground() {
                   loading={<></>}
                   defaultLanguage="html"
                   onChange={setInput}
-                  defaultValue={code}
+                  defaultValue={CODE}
                   value={input}
                   theme={theme}
                 />
