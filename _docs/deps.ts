8ea98619f1e835bad5282deb15df5a2f329fdfb4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-18 18:17:18 +0900

    chore(_docs): remove docs dir and translate anather repo

--- _docs/deps.ts
+++
@@ -1,6 +0,0 @@
-export { default as clsx } from "https://esm.sh/clsx@1.1.1";
-export {
-  isObject,
-  isString,
-} from "https://deno.land/x/isx@v1.0.0-beta.17/mod.ts";
-export { filterValues } from "https://deno.land/std@0.127.0/collections/filter_values.ts";
--- _docs/hooks/use_color_mode_value.ts
+++
@@ -1,9 +0,0 @@
-import { useContext, useMemo } from "react";
-import { DarkModeContext } from "~/contexts/mod.ts";
-
-const useColorModeValue = <Light, Dark>(light: Light, dark: Dark) => {
-  const [isDark] = useContext(DarkModeContext);
-  return useMemo(() => isDark ? dark : light, [isDark]);
-};
-
-export default useColorModeValue;
