cdc0cceb1c2eb7e10b27f1f5ed5c0bb70a273e17
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-27 23:18:24 +0900

    fix(config): change import resource mod to independent file

--- config/extractor.ts
+++ config/extractor.ts
@@ -1,5 +1,5 @@
 import { Extractor } from "./types.ts";
-import { extractSimple } from "../core/mod.ts";
+import { extractSimple } from "../core/extract.ts";
 
 export const SimpleExtractor: Extractor = {
   name: "@mapcss/simple-extractor",
